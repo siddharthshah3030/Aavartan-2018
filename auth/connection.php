b11ed01f2455a30abef03452943eab80adf40fc6
Akash Gayakwad <akashgayakwad123@gmail.com>
2018-09-13 23:58:28 +0530

    auth added

--- auth/connection.php
+++ auth/connection.php
@@ -1,5 +1,5 @@
 <?php
-$link = mysqli_connect("localhost", "Akash", "Akash@123", "onlinenotes");
+$link = mysqli_connect("localhost", "Akash", "Akash@123", "aavartan18");
 if(mysqli_connect_error()){
     die('ERROR: Unable to connect:' . mysqli_connect_error()); 
     echo "<script>window.alert('Hi!')</script>";
