7e0ff008ea808c28f8ba28d025a5bfe9000366a0
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-09-20 16:18:23 +0530

    icons updated

--- header.php
+++ header.php
@@ -1,15 +1,16 @@
             <nav class="outer-nav top horizontal">
-               <a href="/index.php" class="icon-home">Home</a>
-               <a href="/pages/about.php" class="icon-news">About US</a>
-               <a href="/pages/initiative.php" class="icon-news">Initiatives</a>
-               <a href="/pages/vigyaan.php" class="icon-image">Vigyaan</a>
-               <a href="/events/" class="icon-upload">Events</a>
-               <a href="/gallery/" class="icon-star">Gallery</a>
-               <a href="/attractions/" class="icon-mail">Attractions</a>
-               <a href="/auth/signup.php" class="icon-lock">Signup/DashBoard</a>
-               <a href="/pages/sponsor.php" class="icon-lock">Sponsors</a>
-               <a href="/schedule/" class="icon-lock">Schedule</a>
-               <a href="/team/" class="icon-lock">Our Team</a>
-               <a href="/webteam" class="icon-lock">Web Team</a>
-               <a href="/pages/contact.php" class="icon-lock">Contact Us</a>
+               <a href="/index.php" class=""><i class="fa fa-home" aria-hidden="true"></i> Home</a>
+               <a href="/pages/about.php" ><i class="fa fa-info" aria-hidden="true"></i> About US</a>
+               <a href="/pages/initiative.php" ><i class="fa fa-star-o" aria-hidden="true"></i> Initiatives</a>
+               <a href="/pages/vigyaan.php" ><i class="fa fa-flask" aria-hidden="true"></i> Vigyaan</a>
+               <a href="/events/" ><i class="fa fa-futbol-o" aria-hidden="true"></i> Events</a>
+               <a href="/gallery/" ><i class="fa fa-camera" aria-hidden="true"></i> Gallery</a>
+               <a href="/attractions/"><i class="fa fa-trophy" aria-hidden="true"></i> Attractions</a>
+               <a href="/auth/signup.php" ><i class="fa fa-sign-in" aria-hidden="true"></i> Signup/DashBoard</a>
+               <a href="/pages/sponsor.php" ><i class="fa fa-handshake-o" aria-hidden="true"></i> Sponsors</a>
+               <a href="/schedule/" ><i class="fa fa-calendar-times-o" aria-hidden="true"></i> Schedule</a>
+               <a href="/team/" ><i class="fa fa-users" aria-hidden="true"></i> Our Team</a>
+               <a href="/webteam" ><i class="fa fa-user-circle" aria-hidden="true"></i> Web Team</a>
+               <a href="/pages/contact.php" ><i class="fa fa-commenting-o" aria-hidden="true"></i> Contact Us</a>
             </nav>
+
