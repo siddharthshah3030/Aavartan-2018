1f0153779b90dcd967f0f02a73ffa96ef0c14ddc
siddharthshah3030 <siddharthshah3030@gmail.com>
2018-10-04 14:02:41 +0530

    attractions done for now and it all wokrs nwith new menu button

--- newattractions/index.php
+++ newattractions/index.php
@@ -5,9 +5,9 @@
             <link rel="stylesheet" type="text/css" href="css/loader.css">
       <!-- loader begins -->
       <div id = "loaderbody">
-         <div class="stars"></div>
-<div class="twinkling"></div><!--
-<div class="clouds"></div> -->
+         <!-- <div class="stars"></div> -->
+<!-- <div class="twinkling"></div><!-- -->
+<!-- <div class="clouds"></div> --> -->
          <div  class="body">
             <span>
             <span></span>
@@ -28,217 +28,204 @@
          </div>
          <h2>Brace Yourselves</h2>
          <br/>
-         <h1>Reaching Aavartan18</h1>
+         <h1>Awesome Attractions <br>
+         on the way</h1>
       </div>
-        <link rel="stylesheet" type="text/css" href="css/normalize.css" />
-        <link rel="stylesheet" type="text/css" href="css/demo.css" />
-        <link rel="stylesheet" type="text/css" href="css/component.css" />
-        <!-- <link rel="stylesheet" type="text/css" href="css/star.css" /> -->
-        <link rel="stylesheet" type="text/css" href="css/custom.css" />
-        <link rel="stylesheet" type="text/css" href="css/attract.css" />
-        <link rel="stylesheet" type="text/css" href="css/customattract.css" />
-        <link rel="stylesheet" type="text/css" href="css/ghost.css" />
-        <link rel="stylesheet" type="text/css" href="css/title.css" />
-        <!-- <link rel="stylesheet" type="text/css" href="css/background.css"> -->
-              <script src="https://use.fontawesome.com/dbb72ac4f9.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+		<link rel="stylesheet" type="text/css" href="css/demo.css" />
+		<link rel="stylesheet" type="text/css" href="css/component.css" />
+		<!-- <link rel="stylesheet" type="text/css" href="css/star.css" /> -->
+		<link rel="stylesheet" type="text/css" href="css/custom.css" />
+		<!-- <link rel="stylesheet" type="text/css" href="css/background.css"> -->
+		<link rel="stylesheet" type="text/css" href="css/tittle.css" />
+		<link rel="stylesheet" type="text/css" href="css/ghost.css" />
+		<link rel="stylesheet" type="text/css" href="css/attract.css" />
+		<link rel="stylesheet" type="text/css" href="css/customattract.css">
+		<!-- csstransforms3d-shiv-cssclasses-prefixed-teststyles-testprop-testallprops-prefixes-domprefixes-load -->
+		<script src="js/modernizr.custom.25376.js"></script>
+		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	</head>
+	<body style="background: black;" class="particletext confetti">
 
-         <style>
-         .button {
-         width: 110px;
-         padding-top: 5px;
-         padding-bottom: 5px;
-         text-align: center;
-         color: #000;
-         text-transform: uppercase;
-         font-weight: 60;
-         margin-top:8px;
-         margin-left: 30px;
-         margin-bottom: 30px;
-         cursor: pointer;
-         display: inline-block;
-         }
-         .button-1 {
-         background-color: transparent;
-         border: 3px solid #00d7c3;
-         border-radius: 50px;
-         -webkit-transition: all .15s ease-in-out;
-         transition: all .15s ease-in-out;
-         color: #00d7c3;
-         }
-         .button-1:hover {
-         box-shadow: 0 0 10px 0 #00d7c3 inset, 0 0 20px 2px #00d7c3;
-         border: 3px solid #00d7c3;
-         }
-         a h2{
-         color:black;
-     }
-     a p{
-     color:white;
- }
-      </style>
-        <!-- csstransforms3d-shiv-cssclasses-prefixed-teststyles-testprop-testallprops-prefixes-domprefixes-load -->
-        <script src="js/modernizr.custom.25376.js"></script>
-        <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-        <link rel="stylesheet" type="text/css" href="css/team.css">
-    </head>
-    <body style="background: #291B2C;" class="particletext confetti">
-        <div class="w3-row">
-        <div id="perspective" class="perspective effect-laydown">
-            <div class="container" style="background:black;">
-                <div class="wrapper"  >
+    <script src="https://use.fontawesome.com/dbb72ac4f9.js"></script>
+
+    <style>
+     .buttonxyz {
+    width: 110px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    text-align: center;
+    color: #000;
+    text-transform: uppercase;
+    font-weight: 90;
+    margin-top:8px;
+    margin-left: 30px;
+    margin-bottom: 30px;
+    cursor: pointer;
+    display: inline-block;
+    }
+    .buttonxyz-1{
+    background-color: transparent;
+    border: 3px solid #00d7c3;
+    border-radius: 50px;
+    -webkit-transition: all .15s ease-in-out;
+    transition: all .15s ease-in-out;
+    color: #00d7c3;
+    }
+    .buttonxyz-1:hover {
+    box-shadow: 0 0 10px 0 #00d7c3 inset, 0 0 20px 2px #00d7c3;
+    border: 3px solid #00d7c3;
+    }
+       </style>
+		<div class="w3-row">
+		<div id="perspective" class="perspective effect-laydown">
+			<div class="container" style="background:black;">
+				<div class="wrapper"  >
 <!-- wrapper needed for scroll -->
-                <div id="menunavbar" style="margin-bottom:1vw;position:sticky;top:0px;width:100%;background-image: url('test.gif'); height: 100%;   background-position: center;background-repeat: no-repeat;background-size: cover;" >
-                     <button id="showMenu" class="button button-1" style="display: inline-block;float:right;background:black;color:white;text-shadow: 0 0 10px rgba(255,255,255,1) , 0 0 20px rgba(255,255,255,1) , 0 0 30px rgba(255,255,255,1) , 0 0 40px #ff00de , 0 0 70px #ff00de , 0 0 80px #ff00de , 0 0 100px #ff00de ;">Menu</button>
-                     <img src="images/Aavartan.png" height="50px" width="160px" style="display: inline-block;">
-                     <h2 style="color: white; text-shadow: 0 0 10px rgba(255,255,255,0) , 0 0 20px rgba(255,255,255,0) , 0 0 30px rgba(255,255,255,0) , 0 0 40px #00ffff , 0 0 70px #00ffff , 0 0 80px #00ffff , 0 0 100px #00ffff ;"><span></span>Team Technocracy!</h2>
+<div id="menunavbar" style="margin-bottom:1vw;position:sticky;top:0px;width:100%;background-image: url('test.gif'); height: 100px;   background-position: center;background-repeat: no-repeat;background-size: cover;" >
+  <button id="showMenu" class="buttonxyz buttonxyz-1" style="display: inline-block;float:right;background:black;color:white">Menu</button>
+					 <img src="images/Aavartan.png" height="50" width="auto" style="display: inline-block;">
+					 <h2 style="color: white; text-shadow: 0 0 10px rgba(255,255,255,1) , 0 0 20px rgba(255,255,255,1) , 0 0 30px rgba(255,255,255,1) , 0 0 40px #00ffff , 0 0 70px #00ffff , 0 0 80px #00ffff , 0 0 100px #00ffff ;"><span></span>Attractions</h1>
 <br>
 <br>
-                 </div>
-                 
-                 <div id="particles-js">
-                 <div class="cont s--inactive">
+	             </div>
+	<div id="particles-js">
+		<div class="cont s--inactive">
 
-                   <!-- cont inner start -->
-                   <div class="cont__inner">
-                     <!-- el start -->
-                     <div class="el">
-                       <div class="el__overflow">
-                         <div class="el__inner">
-                           <div class="el__bg"></div>
-                           <div class="el__preview-cont">
-                             <h2 class="el__heading">Liquid LED Percussion Drumming Show</h2>
-                           </div>
-                           <div class="el__content">
-                             <div class="el__text">
+			<!-- cont inner start -->
+			<div class="cont__inner">
+				<!-- el start -->
+				<div class="el">
+					<div class="el__overflow">
+						<div class="el__inner">
+							<div class="el__bg"></div>
+							<div class="el__preview-cont">
+								<h2 class="el__heading">Liquid LED <br>Percussion <br>Drumming Show</h2>
+							</div>
+							<div class="el__content">
+								<div class="el__text">
+                  Have you ever seen imagination coming true ? Have you ever seen lights ,liquids and thumping together?<br>
+                  Well if you haven't, the time has come to witness the most extravagant event.<br>
+                  Team Technocarcy presents you with the spectacular ,sumptous "Liquid LED Percussion Drumming Show".<br>
+                  It is the best live music entertainment in India.<br>
 
-                               Have you ever seen imagination coming true ? Have you ever seen lights ,liquids and thumping together?
-                               Well if you haven't, the time has come to witness the most extravagant event.
-                               Team Technocarcy presents you with the spectacular ,sumptous "Liquid LED Percussion Drumming Show".
-                               It is the best live music entertainment in India.
+                <br>  Come , join us this AAVARTAN and watch your imagination getting life.
 
-                               Come , join us this AAVARTAN and watch your imagination getting life.
+                <br>  6th October || 7:00 pm || Main Stage								</div>
+								<div class="el__close-btn"></div>
+							</div>
+						</div>
+					</div>
+					<div class="el__index">
+						<div class="el__index-back">1</div>
+						<div class="el__index-front">
+							<div class="el__index-overlay" data-index="1">1</div>
+						</div>
+					</div>
+				</div>
+				<!-- el end -->
+				<!-- el start -->
+				<div class="el">
+					<div class="el__overflow">
+						<div class="el__inner">
+							<div class="el__bg"></div>
+							<div class="el__preview-cont">
+								<h2 class="el__heading">MJ 5</h2>
+							</div>
+							<div class="el__content">
+								<div class="el__text">
+                  All the stars are aligned and the sky is set to make the whole Cosmo whirl to the rhythm of beats, as the dancing shoes will make the movement.
+      <br> So Amigos!! We are here to amaze you to the brim of enthusiasm as AAVARTAN'18 brings you the Moonwalk Magicians, champions of India's Dancing Superstar 2013, an idolization of Michael Jackson MJ5 for the very first time to NIT Raipur this AAVARTAN'18!!
 
-                               6th October || 7:00 pm || Main Stage								</div>
-                             <div class="el__close-btn"></div>
-                           </div>
-                         </div>
-                       </div>
-                       <div class="el__index">
-                         <div class="el__index-back">1</div>
-                         <div class="el__index-front">
-                           <div class="el__index-overlay" data-index="1">1</div>
-                         </div>
-                       </div>
-                     </div>
-                     <!-- el end -->
-                     <!-- el start -->
-                     <div class="el">
-                       <div class="el__overflow">
-                         <div class="el__inner">
-                           <div class="el__bg"></div>
-                           <div class="el__preview-cont">
-                             <h2 class="el__heading">MJ 5</h2>
-                           </div>
-                           <div class="el__content">
-                             <div class="el__text">
-                               All the stars are aligned and the sky is set to make the whole Cosmo whirl to the rhythm of beats, as the dancing shoes will make the movement.
-                 So Amigos!! We are here to amaze you to the brim of enthusiasm as AAVARTAN'18 brings you the Moonwalk Magicians, champions of India's Dancing Superstar 2013, an idolization of Michael Jackson MJ5 for the very first time to NIT Raipur this AAVARTAN'18!!
+       <br>Be there & witness the splendid evening!
 
-                 Be there & witness the splendid evening!
+       <br>7th October || 7:00 pm || Main Stage, NIT RAIPUR
 
-                 7th October || 7:00 pm || Main Stage, NIT RAIPUR
+      <br> #AAVARTAN18 #MJ5 #Techfest #NITRR              </div>
+								<div class="el__close-btn"></div>
+							</div>
+						</div>
+					</div>
+					<div class="el__index">
+						<div class="el__index-back">2</div>
+						<div class="el__index-front">
+							<div class="el__index-overlay" data-index="2">2</div>
+						</div>
+					</div>
+				</div>
+				<!-- el end -->
+				<!-- el start -->
+				<!-- <div class="el">
+					<div class="el__overflow">
+						<div class="el__inner">
+							<div class="el__bg"></div>
+							<div class="el__preview-cont">
+								<h2 class="el__heading">SAND ART</h2>
+							</div>
+							<div class="el__content">
+								<div class="el__text">
+									The Sand Gallery this AAVARTAN would be setup by one of the most talented artist of India - Mr. Vivek Patil. He is the finalist of India's Got Talent Season 5.Sand Art is actually drawing in sand by hands. All this is happening on a specially designed table, which has under lighted glass top.
+</div>
+								<div class="el__close-btn"></div>
+							</div>
+						</div>
+					</div>
+					<div class="el__index">
+						<div class="el__index-back">3</div>
+						<div class="el__index-front">
+							<div class="el__index-overlay" data-index="3">3</div>
+						</div>
+					</div>
+				</div> -->
+				<!-- el end -->
+				<!-- el start -->
+				<!-- <div class="el">
+					<div class="el__overflow">
+						<div class="el__inner">
+							<div class="el__bg"></div>
+							<div class="el__preview-cont">
+								<h2 class="el__heading">LASER SHOW</h2>
+							</div>
+							<div class="el__content">
+								<div class="el__text">
+									Get ready to witness a Laser Show that will be performed by Laser ShowIndia, India's First & No.1 3D Laser Show company. Laser show will feature dazzling colourful laser animations, cartoons and beam effects all choreographed to a musical soundtrack and theme.
+</div>
+								<div class="el__close-btn"></div>
+							</div>
+						</div>
+					</div>
+					<div class="el__index">
+						<div class="el__index-back">4</div>
+						<div class="el__index-front">
+							<div class="el__index-overlay" data-index="4">4</div>
+						</div>
+					</div>
+				</div> -->
+				<!-- el end -->
+				<!-- el start -->
 
-                 #AAVARTAN18 #MJ5 #Techfest #NITRR            </div>
-                             <div class="el__close-btn"></div>
-                           </div>
-                         </div>
-                       </div>
-                       <div class="el__index">
-                         <div class="el__index-back">2</div>
-                         <div class="el__index-front">
-                           <div class="el__index-overlay" data-index="2">2</div>
-                         </div>
-                       </div>
-                     </div>
-                     <!-- el end -->
-                     <!-- el start -->
-                     <div class="el">
-                       <div class="el__overflow">
-                         <div class="el__inner">
-                           <div class="el__bg"></div>
-                           <div class="el__preview-cont">
-                             <h2 class="el__heading">SAND ART</h2>
-                           </div>
-                           <div class="el__content">
-                             <div class="el__text">
-                               The Sand Gallery this AAVARTAN would be setup by one of the most talented artist of India - Mr. Vivek Patil. He is the finalist of India's Got Talent Season 5.Sand Art is actually drawing in sand by hands. All this is happening on a specially designed table, which has under lighted glass top.
-                 </div>
-                             <div class="el__close-btn"></div>
-                           </div>
-                         </div>
-                       </div>
-                       <div class="el__index">
-                         <div class="el__index-back">3</div>
-                         <div class="el__index-front">
-                           <div class="el__index-overlay" data-index="3">3</div>
-                         </div>
-                       </div>
-                     </div>
-                     <!-- el end -->
-                     <!-- el start -->
-                     <div class="el">
-                       <div class="el__overflow">
-                         <div class="el__inner">
-                           <div class="el__bg"></div>
-                           <div class="el__preview-cont">
-                             <h2 class="el__heading">LASER SHOW</h2>
-                           </div>
-                           <div class="el__content">
-                             <div class="el__text">
-                               Get ready to witness a Laser Show that will be performed by Laser ShowIndia, India's First & No.1 3D Laser Show company. Laser show will feature dazzling colourful laser animations, cartoons and beam effects all choreographed to a musical soundtrack and theme.
-                 </div>
-                             <div class="el__close-btn"></div>
-                           </div>
-                         </div>
-                       </div>
-                       <div class="el__index">
-                         <div class="el__index-back">4</div>
-                         <div class="el__index-front">
-                           <div class="el__index-overlay" data-index="4">4</div>
-                         </div>
-                       </div>
-                     </div>
-                     <!-- el end -->
-                     <!-- el start -->
+				<!-- el end -->
+			</div>
+			<!-- cont inner end -->
+		</div>
+		<div class="ghostcontainer">
+			<div class="ghosttext"></div>
+		</div></div>
 
-                     <!-- el end -->
-                   </div>
-                   <!-- cont inner end -->
-                 </div>
-                 <div class="ghostcontainer">
-                   <div class="ghosttext"></div>
-                 </div></div>
-
-
-
-
-    <!--    <svg width="0" height="0">
-          <defs>
-            <clipPath id="hexagono" clipPathUnits="objectBoundingBox">
-              <polygon points=".25 0, .75 0, 1 .5, .75 1, .25 1, 0 .5" />
-            </clipPath>
-          </defs>
-        </svg> -->
     </div><!-- wrapper -->
     </div><!-- /container -->
-            <a id="menuclose" href="#" ><span class="close">&times;</span></a>
-            <?php include '../header.php' ?>
-        </div><!-- /perspective -->
-    </div>
+    <a id="menuclose" href="#" ><span class="close">&times;</span></a>
+    <?php include '../header.php' ?>
+  </div><!-- /perspective -->
+  </div>
 <!-- particles.js lib - https://github.com/VincentGarreau/particles.js -->
-        <script src="js/classie.js"></script>
-        <script src="js/menu.js"></script>
-        <script src="js/custom.js"></script>
-        <script src="js/loader.js"></script>
-    </body>
+<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+		<script src="js/classie.js"></script>
+		<script src="js/menu.js"></script>
+		<script src="js/attract.js"></script>
+		<script src="js/ghostheading.js"></script>
+		<script src="js/custom.js"></script>
+		<script  src="js/index.js"></script>
+		<script  src="js/loader.js"></script>
+	</body>
 </html>
